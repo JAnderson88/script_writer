b3ae9f84748611c3c101ca96e42bc37f02dca2c3
JAnderson88 <jande081@fiu.edu>
2020-06-23 00:00:12 -0400

    Initial Commit

---
+++ src/Layout/Header/header.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+//CSS
+import '../Header/header.css';
+
+function Header() {
+  return (
+    <Fragment>
+      <header></header>
+    </Fragment>
+  )
+}
+
+export default Header;
---
+++ src/Routes/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//Layout
+import Header from '../Layout/Header/header';
+
+
+function Home() {
+  return (
+    <div id="home">
+      <Header />
+    </div>
+  )
+}
+
+export default Home;
